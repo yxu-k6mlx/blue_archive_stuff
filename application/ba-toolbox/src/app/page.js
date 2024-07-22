8314a36b67ff25d2a8b0ec3d5bb83278d1b3cf63
k6mlx <xuyanwen2xz@gmail.com>
2024-07-21 19:31:13 -0700

    changed front page

--- application/ba-toolbox/src/app/page.js
+++ application/ba-toolbox/src/app/page.js
@@ -3,11 +3,69 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <body>
-      <section>
-        <h1>Hello world! </h1>
-        <p></p>
-      </section>
-    </body>
+    <main className={styles.main}>
+      <div className={styles.center}>
+        <Image
+          className={styles.logo}
+          src="/ba_toolbox_logo.png"
+          alt="Hi :)"
+          width={1086}
+          height={350}
+          priority
+        />
+      </div>
+
+      <div className={styles.grid}>
+        <a
+          href="https://yxu-k6mlx.github.io/"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer" 
+        >
+          <h2>
+            My Page <span>-&gt;</span>
+          </h2>
+          <p>Find out more about me and my contacts. </p>
+        </a>
+
+        <a
+          href="https://github.com/yxu-k6mlx/blue_archive_stuff/"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            GitHub Page <span>-&gt;</span>
+          </h2>
+          <p>Track progress and contribute to this project. </p>
+        </a>
+
+        <a
+          href="https://yxu-k6mlx.github.io/Projects/readme.html"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Other Projects <span>-&gt;</span>
+          </h2>
+          <p>See my other projects. </p>
+        </a>
+
+        <a
+          href=""
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            简体中文站 <span>-&gt;</span>
+          </h2>
+          <p>
+            施工中.
+          </p>
+        </a>
+      </div>
+    </main>
   );
 }
