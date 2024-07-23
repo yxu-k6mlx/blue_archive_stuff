268cf568d0215fa273e9872414a86d536b8ccb7d
k6mlx <xuyanwen2xz@gmail.com>
2024-07-22 20:44:36 -0700

    updated name on page.js

--- application/ba-toolbox/src/app/page.js
+++ application/ba-toolbox/src/app/page.js
@@ -1,11 +1,11 @@
-import MainApp from "./logo"
+import Logo from "./logo"
 import Directions from "./directions"
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <MainApp />
+      <Logo />
       <Directions />
     </main>
   );
