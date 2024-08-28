fe2f80fcff40768abd6d45561d7237938f8aba0c
myland123456 <xuyanwen2xz@gmail.com>
2024-08-27 18:47:01 -0700

    buttonbar working

--- ba-tools/gui/lowerscreen/buttons.py
+++ ba-tools/gui/lowerscreen/buttons.py
@@ -18,12 +18,14 @@ class Askew_Button(tk.Frame):
         self._w = tk.Canvas(root, height=height+pady, width=width+x_offset+padx)
 
         self.width = width 
-        self.height = height
+        self.height = height 
 
         self.Ax, self.Ay = x + x_offset + padx, y+pady
         self.Bx, self.By = self.Ax + self.width, self.Ay
         self.Cx, self.Cy = self.Bx - x_offset, self.Ay + self.height
         self.Dx, self.Dy = x + padx, y + self.height + pady
+
+        print(f'ABCD: {self.Ax, self.Ay}, {self.Bx, self.By}, {self.Cx, self.Cy}, {self.Dx, self.Dy}')
         
         self._w.create_polygon(
             self.Ax, self.Ay, 
@@ -35,13 +37,22 @@ class Askew_Button(tk.Frame):
 
         self._w.create_text(
             (self.Bx-self.Dx)/2, (self.Dy-self.Ay)/2, 
-            justify='center', text="AUTO", font=('Arial Narrow', height-pady, 'bold italic'), 
+            justify='center', text=text, font=('Arial Narrow', height-pady, 'bold italic'), 
             fill='blue'
         )
 
+class ControlBar(tk.Frame): 
+    def __init__(self, root, x=0, y=0, height=50, width=100, x_offset=2, padx=5, pady=5):
+        self.tk = root 
+        self._w = tk.Canvas(root, height=(height+pady), width=2*(width+x_offset+padx) + 100)
+        auto = Askew_Button(test_root, x=0, y=0, height=100, width=300, x_offset=40, text='AUTO')
+        menu = Askew_Button(test_root, x=0, y=0, height=100, width=300, x_offset=40, text='MENU')
+        auto.grid(column=0, row=0)
+        menu.grid(column=1, row=0)
+
 if __name__ == '__main__': 
     test_root = tk.Tk()
-    test_button = Askew_Button(test_root, x=0, y=0, height=100, width=300, x_offset=40)
+    test_button = ControlBar(test_root, x=0, y=0, height=100, width=300, x_offset=40)
     test_button.place(x=0, y=0)
     test_root.title('Component Tester')
     test_root.mainloop() 
