2055305c0a0db1e12909c68a9bb156c2b54a4a80
k6mlx <xuyanwen2xz@gmail.com>
2024-08-28 21:05:37 -0700

    display empty frame

--- ba-tools/gui/middlescreen/display.py
+++ ba-tools/gui/middlescreen/display.py
@@ -1,9 +1,24 @@
 import tkinter as tk
 
 class DispArea(tk.Frame): 
-    def __init__(self, root): 
+    def __init__(self, root, v_limit=0, h_limit=0): 
+        self.tk = root 
+        self.width, self.height = self.calculate_position(v_limit=v_limit, h_limit=h_limit)
+
+        self._w = tk.Canvas(root, width=self.width, height=self.height, bg='black') 
         
-        pass
+        self._w.grid(column=0,row=0)
+
+    def text_to_disp(self, root, text): 
+        self.text = text 
+        self.label = tk.Label(root, text=self.text, fg='white', font=('Noto Sans', 50, 'bold'))
+
+    def calculate_position(self, v_limit, h_limit): 
+        v_ratio = 0.4
+        return (1-v_ratio)*v_limit, h_limit
 
 if __name__ == '__main__': 
-    pass
+    test = tk.Tk() 
+    disp_area = DispArea(test, v_limit=1000, h_limit=200)
+    
+    test.mainloop()
