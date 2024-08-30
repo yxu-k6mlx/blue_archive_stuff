243be6a0868688cd36df348606a0e04a587098ff
k6mlx <xuyanwen2xz@gmail.com>
2024-08-29 17:42:47 -0700

    added a constant

--- ba-tools/gui/lowerscreen/namecard.py
+++ ba-tools/gui/lowerscreen/namecard.py
@@ -1,12 +1,12 @@
 import tkinter as tk
-    
+
+NAME_SPLIT_WIDTH = 20    
+
 class Namecard(tk.Frame): 
-    def __init__(self, root, height=0, width=0, window_width=100): 
+    def __init__(self, root, height=0, width=0, window_width=10): 
         self.name_str = 'Student'
         self.from_str = 'Affiliation'
-        self.bg = '
-'
-
+        self.bg = 'red'
         self.tk = root
         self._w = tk.Canvas(root, bg='black', height=height, width=width, highlightthickness=0)
 
@@ -15,17 +15,18 @@ class Namecard(tk.Frame):
             text=self.name_str, fg='#fefefe', bg='black', highlightthickness=0
         )
 
+        self.pad = tk.Canvas(self._w, height=height, width=NAME_SPLIT_WIDTH, bg='black', highlightthickness=0)
+
         self.from_name = tk.Label(
             self._w, justify='left', font=('Noto Sans', 40),
             text=self.from_str, fg='#aaddff', pady=5, bg='black', highlightthickness=0
         )
 
-        self.rightpad = tk.Canvas(height=height, width=width, bg='black', highlightthickness=0)
-        self.pad = tk.Canvas(self._w, height=height, width=5, bg='black', highlightthickness=0)
+        #self.rightpad = tk.Canvas(height=height, width=width, bg='black', highlightthickness=0)
+        
 
         self.line_length = window_width
-        
-        self.line = tk.Canvas(root, width=self.line_length, height=2, bg='#dddddd', highlightthickness=0)
+        #self.line = tk.Canvas(root, width=self.line_length, height=2, bg='red', highlightthickness=0)
         
         self.set_to_disp()
 
@@ -33,8 +34,8 @@ class Namecard(tk.Frame):
         self.char_name.grid(column=0, row=0, sticky='sw')
         self.pad.grid(column=1, row=0)
         self.from_name.grid(column=2, row=0, sticky='sw')
-        self.rightpad.grid(column=3, row=0)
-        self.line.grid(row=1)
+        #self.rightpad.grid(column=3, row=0, sticky='sw')
+        #self.line.grid(row=1)
         self.configure(bg='black')
 
 if __name__ == '__main__': 
