2e14346890aa7ef145581d23c5dc7f3a085cfa63
myland123456 <xuyanwen2xz@gmail.com>
2024-08-30 00:03:49 -0700

    using png is cheating but works now onto dynamic resize and placements

--- ba-tools/gui/lowerscreen/dialog.py
+++ ba-tools/gui/lowerscreen/dialog.py
@@ -4,11 +4,19 @@ from PIL import Image, ImageTk
 
 class DialogBackdrop(tk.Frame): 
     def __init__(self, root): 
-        self.frame = tk.Canvas(root, width=1600, height=1200)
-        self.image = ImageTk.PhotoImage(file='blue_archive_stuff/ba-tools/assets/ui/DialogDisplay/text_backdrop.png')
-        self.image = ImageTk.PhotoImage(file='')
-        self.frame.create_image(0, 0, image=self.image)
-        self.frame.pack() 
+        self.canvas = tk.Canvas(root, width=1200, height=900)
+
+        self.bdimg = Image.open('blue_archive_stuff/ba-tools/assets/ui/DialogDisplay/text_backdrop.png')
+        self.bdimg = self.bdimg.resize((1000, 400), Image.Resampling.LANCZOS)
+        self.tkbd = ImageTk.PhotoImage(self.bdimg)
+
+        self.bgimg = Image.open('blue_archive_stuff/ba-tools/assets/data/Background/BG_HQ.jpg')
+        self.bgimg.resize((1200, 900), Image.Resampling.LANCZOS)
+        self.tkbg = ImageTk.PhotoImage(self.bgimg)
+        
+        self.canvas.create_image(1200/2, 900/2, image=self.tkbg)
+        self.canvas.create_image(1200/2, 900/2+200, image=self.tkbd)
+        self.canvas.grid(column=0,row=0)
 
 if __name__ == '__main__': 
     test = tk.Tk()
