e92ec3e387f6821c40fe796ed095e4dfb06f4982
k6mlx <xuyanwen2xz@gmail.com>
2024-08-30 15:49:00 -0700

    text dialog disp mockup good TODO: transparency

--- ba-tools/gui/gui.py
+++ ba-tools/gui/gui.py
@@ -1,13 +1,15 @@
 import tkinter as tk
-from lowerscreen.dialog import DialogBackdrop
+from lowerscreen.dialog import DialogBackdrop, DialogText
 from lowerscreen.namecard import Namecard
 
 if __name__ == '__main__': 
     test = tk.Tk()
     backdrop = DialogBackdrop(test, bg_image_path='blue_archive_stuff/ba-tools/assets/data/Background/BG_HQ.jpg')
     namecard = Namecard(test, height=100, width=200, window_width=500)
-    
+    dialog = DialogText(test, line0='这是第一行字', line1='This is line 2', line2='0 1 2 3 4 5 6 7 8 9 10 11 12')
+
     backdrop.pack()
     namecard.place(x=100, y=500)
-
+    dialog.place(x=100, y=600)
+    
     tk.mainloop()
--- ba-tools/gui/lowerscreen/dialog.py
+++ ba-tools/gui/lowerscreen/dialog.py
@@ -17,10 +17,29 @@ class DialogBackdrop(tk.Frame):
         self._w.create_image(1200/2, 900/2, image=self.tkbg)
         self._w.create_image(1200/2, 900/2+185, image=self.tkbd)
 
+
+class DialogText(tk.Frame): 
+    def __init__(self, root, line0='', line1='', line2=''): 
+        self.tk = root 
+        self._w = tk.Canvas(root, width=1200, height=400, bg='purple', highlightthickness=0)
+
+        self.y_offset = 1000
+
+        self.line0 = tk.Label(self._w, text=line0, font=('Noto Sans', 40), fg='#fefefe', bg='black', padx=0, pady=0, justify='left')
+        self.line1 = tk.Label(self._w, text=line1, font=('Noto Sans', 40), fg='#fefefe', bg='black', padx=0, pady=0, justify='left')
+        self.line2 = tk.Label(self._w, text=line2, font=('Noto Sans', 40), fg='#fefefe', bg='black', padx=0, pady=0, justify='left')
+
+        self.line0.grid(column=0, row=0, sticky='sw')
+        self.line1.grid(column=0, row=1, sticky='sw')
+        self.line2.grid(column=0, row=2, sticky='sw')
+        
+
 if __name__ == '__main__': 
     test = tk.Tk()
     test.title('DialogBackdrop Test')
     backdrop = DialogBackdrop(test, bg_image_path='blue_archive_stuff/ba-tools/assets/data/Background/BG_HQ.jpg')
+    text = DialogText(test, line0='Line0', line1='Line1', line2='Line 2')
+    text.place(x=0, y=0)
     test.resizable(True, True)
     test.mainloop()
     
