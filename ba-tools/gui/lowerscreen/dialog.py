e1d2a988ba0403c5b77d120db7cbca68b112fb05
k6mlx <xuyanwen2xz@gmail.com>
2024-08-30 15:13:31 -0700

    dialog bd working(?)

--- ba-tools/gui/lowerscreen/dialog.py
+++ ba-tools/gui/lowerscreen/dialog.py
@@ -7,7 +7,7 @@ class DialogBackdrop(tk.Frame):
         self.canvas = tk.Canvas(root, width=1200, height=900)
 
         self.bdimg = Image.open('blue_archive_stuff/ba-tools/assets/ui/DialogDisplay/text_backdrop.png')
-        self.bdimg = self.bdimg.resize((1000, 400), Image.Resampling.LANCZOS)
+        self.bdimg = self.bdimg.resize((1025, 400), Image.Resampling.LANCZOS)
         self.tkbd = ImageTk.PhotoImage(self.bdimg)
 
         self.bgimg = Image.open('blue_archive_stuff/ba-tools/assets/data/Background/BG_HQ.jpg')
@@ -15,7 +15,7 @@ class DialogBackdrop(tk.Frame):
         self.tkbg = ImageTk.PhotoImage(self.bgimg)
         
         self.canvas.create_image(1200/2, 900/2, image=self.tkbg)
-        self.canvas.create_image(1200/2, 900/2+200, image=self.tkbd)
+        self.canvas.create_image(1200/2, 900/2+185, image=self.tkbd)
         self.canvas.grid(column=0,row=0)
 
 if __name__ == '__main__': 
