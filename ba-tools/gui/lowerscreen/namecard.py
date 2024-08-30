2169c11d6c587ea09bc8d4ee5bbb26f68fd921f0
k6mlx <xuyanwen2xz@gmail.com>
2024-08-30 15:26:43 -0700

    adjusted background color for debugging bg: black -> purple

--- ba-tools/gui/lowerscreen/namecard.py
+++ ba-tools/gui/lowerscreen/namecard.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 
-NAME_SPLIT_WIDTH = 20    
+NAME_SPLIT_WIDTH = 20
+LABEL_BG = 'purple'
 
 class Namecard(tk.Frame): 
     def __init__(self, root, height=0, width=0, window_width=10): 
@@ -8,18 +9,18 @@ class Namecard(tk.Frame):
         self.from_str = 'Affiliation'
         self.bg = 'red'
         self.tk = root
-        self._w = tk.Canvas(root, bg='black', height=height, width=width, highlightthickness=0)
+        self._w = tk.Canvas(root, bg=LABEL_BG, height=height, width=width, highlightthickness=0)
 
         self.char_name = tk.Label(
-            self._w, justify='left', font=('Noto Sans', 50), 
-            text=self.name_str, fg='#fefefe', bg='black', highlightthickness=0
+            self._w, justify='left', font=('Noto Sans', 40), 
+            text=self.name_str, fg='#fefefe', bg=LABEL_BG, highlightthickness=0
         )
 
-        self.pad = tk.Canvas(self._w, height=height, width=NAME_SPLIT_WIDTH, bg='black', highlightthickness=0)
+        self.pad = tk.Canvas(self._w, height=height, width=NAME_SPLIT_WIDTH, bg=LABEL_BG, highlightthickness=0)
 
         self.from_name = tk.Label(
-            self._w, justify='left', font=('Noto Sans', 40),
-            text=self.from_str, fg='#aaddff', pady=5, bg='black', highlightthickness=0
+            self._w, justify='left', font=('Noto Sans', 30),
+            text=self.from_str, fg='#aaddff', pady=5, bg=LABEL_BG, highlightthickness=0
         )
 
         #self.rightpad = tk.Canvas(height=height, width=width, bg='black', highlightthickness=0)
@@ -36,7 +37,7 @@ class Namecard(tk.Frame):
         self.from_name.grid(column=2, row=0, sticky='sw')
         #self.rightpad.grid(column=3, row=0, sticky='sw')
         #self.line.grid(row=1)
-        self.configure(bg='black')
+        self.configure(bg='purple')
 
 if __name__ == '__main__': 
     active_speaker = None # coming soon with: char sprite update! 
