9260a318a59262db5f783ab6af4a4b0f49807751
myland123456 <xuyanwen2xz@gmail.com>
2024-08-29 23:11:39 -0700

    untested dialog backdrop png method

--- ba-tools/gui/lowerscreen/dialog.py
+++ ba-tools/gui/lowerscreen/dialog.py
@@ -1,4 +1,19 @@
 import tkinter as tk
 from gradient import GradientFrame
+from PIL import Image, ImageTk
 
-class DialogBackdrop()
+class DialogBackdrop(tk.Frame): 
+    def __init__(self, root): 
+        self.frame = tk.Canvas(root, width=1600, height=1200)
+        self.image = ImageTk.PhotoImage(file='blue_archive_stuff/ba-tools/assets/ui/DialogDisplay/text_backdrop.png')
+        self.image = ImageTk.PhotoImage(file='')
+        self.frame.create_image(0, 0, image=self.image)
+        self.frame.pack() 
+
+if __name__ == '__main__': 
+    test = tk.Tk()
+    test.title('DialogBackdrop Test')
+    backdrop = DialogBackdrop(test)
+    test.resizable(True, True)
+    test.mainloop()
+    
--- ba-tools/main.py
+++ ba-tools/main.py
@@ -4,10 +4,21 @@ from pathlib import Path
 # that is the ba-tools folder
 ROOT_PATH = Path(__file__).parent 
 
+def resize(event):
+    global window_width, window_height
+    if event.widget.widgetName == "toplevel":
+        if (window_width != event.width) and (window_height != event.height):
+            window_width, window_height = event.width,event.height
+            print(f"The width of Toplevel is {window_width} and the height of Toplevel "
+                  f"is {window_height}")
+
+window_width, window_height = 0, 0
+
 if __name__ == '__main__': 
     root = tk.Tk()
-    screen_width = root.winfo_screenwidth()
-    screen_height = root.winfo_screenheight()
-
+    root.resizable(True, True)
+    top = tk.Toplevel()
+    root.bind("<Configure>", resize)
     tk.mainloop()
+
 
